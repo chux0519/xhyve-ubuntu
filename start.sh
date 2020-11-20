9c1712556bea8bc6c799f5b827fb1208493398ba
chux0519 <chuxdesign@hotmail.com>
2020-11-20 16:23:37 +0800

    enable cgroupv2

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ cpus=1
 
 xhyve \
     -A \
-	-U "$(cat uuid.xhyve)" \
+	-U "5142aefe-220f-4e9a-9bb3-ee09b7214688" \
     -c "$cpus" \
     -m "${memgb}G" \
     -s 0,hostbridge \
@@ -21,4 +21,4 @@ xhyve \
     -s 4,virtio-blk,storage.img \
     -s 31,lpc \
     -l com1,stdio \
-	-f "kexec,boot/vmlinuz-5.4.0-54-generic,boot/initrd.img-5.4.0-54-generic,earlyprintk=serial console=ttyS0 root=/dev/vda2 ro"
+	-f "kexec,boot/vmlinuz-5.4.0-54-generic,boot/initrd.img-5.4.0-54-generic,earlyprintk=serial console=ttyS0 root=/dev/vda2 ro systemd.unified_cgroup_hierarchy=1"
